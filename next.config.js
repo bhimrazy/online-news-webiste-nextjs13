a26ae3015c367f9f53b1c8f22e3bb37d834ba3eb
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2023-04-22 13:01:16 +0000

    Next js setup

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["images.unsplash.com"]
+
+  }
+}
+
+module.exports = nextConfig
